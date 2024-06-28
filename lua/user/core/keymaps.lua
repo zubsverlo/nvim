8a2667aab21224e0d08e3a7f3a34b461d9b0bf41
zubsverlo <zub.sverlo@gmail.com>
2024-06-28 15:26:25 +0500

    date keymaps

--- lua/user/core/keymaps.lua
+++ lua/user/core/keymaps.lua
@@ -11,3 +11,7 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to prev tab
 
+-- insert [d]ate, [t]ime or [f]ull date and time
+keymap.set("n", "<leader>dd", "a<c-r>=strftime('%d.%m')<CR><Esc>")
+keymap.set("n", "<leader>dt", "a<c-r>=strftime('%H:%M')<CR><Esc>")
+keymap.set("n", "<leader>df", "a<c-r>=strftime('%d.%m %H:%M')<CR><Esc>")
